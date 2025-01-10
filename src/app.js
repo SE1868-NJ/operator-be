9bd1d932fd935c0d7f5a8863e57c2ea3984c400c
hoshikira18 <khuyen.dev183@gmail.com>
2025-01-11 02:01:58 +0700

    chore: add folder structure

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 // config .ENV
-require('dotenv').config({ path: './.env' });
+require("dotenv").config({ path: "./.env" });
 
 const PORT = 3000;
 
-const cors = require('cors');
-const morgan = require('morgan');
-const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const morgan = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const corsOptions = {
-    origin: '*',
+    origin: "*",
 };
 
 app.use(cors(corsOptions));
 
 // setup terminal logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.use(bodyParser.json());
 app.use(
@@ -26,10 +26,26 @@ app.use(
     }),
 );
 
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    database: "db",
+});
+
+db.connect();
+
+app.get("/", (req, res) => {
+    db.query("create table dba(id int)");
+    res.json({
+        message: "hello",
+    });
+});
+
 app.listen(PORT, (err) => {
     if (!err) {
         console.log(`Server is running on http://localhost:${PORT}`);
     } else {
-        console.log('Error occurred, server can not start', err);
+        console.log("Error occurred, server can not start", err);
     }
 });
