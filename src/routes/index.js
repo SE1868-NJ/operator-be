85c1bd5f2ef08ce6213317ce0ee4ddfe95785ee8
hoshikira18 <khuyen.dev183@gmail.com>
2025-02-18 05:35:49 +0700

    feat: notifications

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import authRouter from "./auth.route.js";
+import notifRouter from "./notifications.route.js";
+import reportsRouter from "./reports.route.js";
 import shipperRouter from "./shipper.route.js";
 import shopRouter from "./shop.route.js";
-import userRouter from "./user.route.js";
 
 const route = (app) => {
     app.use("/auth", authRouter);
     app.use("/admin", authRouter);
     app.use("/shippers", shipperRouter);
     app.use("/shops", shopRouter);
+    app.use("/reports", reportsRouter);
+    app.use("/notification", notifRouter);
 };
 
 export default route;
