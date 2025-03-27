30e019625b7d1d0af47b560c778e9154eb7d9cf7
Meocute5104 <nguyendangkhoa5104@gmail.com>
2025-03-28 00:08:53 +0700

    refactor: operator role model

--- src/models/operator.model.js
+++ src/models/operator.model.js
@@ -10,10 +10,16 @@ export const Operator = sequelize.define("Operator", {
     avatar: {
         type: DataTypes.STRING,
         allowNull: false,
-        defaultValue: "https://i.pinimg.com/236x/5e/e0/82/5ee082781b8c41406a2a50a0f32d6aa6.jpg",
+        defaultValue:
+            "https://res.cloudinary.com/dafn4ktx0/image/upload/v1741914627/default_avatar_zqgyov.jpg",
         validate: {
             notEmpty: true,
         },
+    },
+    avatar_public_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "swp_storage_image/default_avatar_zqgyov",
     },
     firstName: {
         type: DataTypes.STRING,
@@ -27,6 +33,14 @@ export const Operator = sequelize.define("Operator", {
         allowNull: false,
         validate: {
             notEmpty: true,
+        },
+    },
+    personalEmail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
         },
     },
     email: {
@@ -49,25 +63,21 @@ export const Operator = sequelize.define("Operator", {
         allowNull: false,
         validate: {
             notEmpty: true,
-            len: [10, 15],
+            len: [10, 15], // Số điện thoại từ 10-15 ký tự
         },
     },
     dateOfBirth: {
-        type: DataTypes.DATEONLY,
+        type: DataTypes.DATEONLY, // Chỉ lưu ngày (YYYY-MM-DD)
         allowNull: false,
     },
     gender: {
-        type: DataTypes.ENUM("male", "female", "other"),
+        type: DataTypes.ENUM("male", "female", "other"), // Giới tính có thể là Nam, Nữ hoặc Khác
         allowNull: false,
     },
     status: {
-        type: DataTypes.ENUM("active", "inactive"),
+        type: DataTypes.ENUM("active", "inactive", "pending"),
         allowNull: false,
-        defaultValue: "active",
-    },
-    roleCode: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+        defaultValue: "pending",
     },
 });
 
